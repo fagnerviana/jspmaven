2e6f680a4f9b1b94df2eb2a979bc81f5c3dfbbdb
fagnerviana <fagsantiago@gmail.com>
2022-11-23 17:52:24 -0300

    Criando o Logout e encerramento da do usuário no sistema

--- src/servlets/ServletLogin.java
+++ src/servlets/ServletLogin.java
@@ -29,7 +29,21 @@ public class ServletLogin extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	doPost(request, response);		
+	
+		String acao = request.getParameter("acao");
+		
+		if(acao !=null && !acao.isEmpty() && acao.equalsIgnoreCase("logout")) {
+			
+			request.getSession().invalidate(); //invalida a sessão 
+			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+			redirecionar.forward(request, response);
+			
+		}else {
+			
+			doPost(request, response);
+			
+		}
+				
 		
 	}
 
